384dc02079bce0b2ac03353894f38c6f3444a5d5
unknown <sanaebounajma7@gmail.com>
2023-03-08 14:34:48 +0100

    7

---
+++ tests.py
@@ -0,0 +1,8 @@
+from main import * 
+import pytest
+from unittest import TestCase
+def test_minus_true(self):
+        self.assertEqual(soustraction(2, 1), 1)
+
+def test_minus_wrong(self):
+        self.assertNotEqual(soustraction(2,1),2)
