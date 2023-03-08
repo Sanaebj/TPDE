96e78f4734d047673ee9daf5caca7d202ecb7dc3
unknown <sanaebounajma7@gmail.com>
2023-03-08 15:14:53 +0100

    f

--- tests.py
+++ tests.py
@@ -6,4 +6,4 @@ def test_minus_true(self):
         self.assertEqual(soustraction(2, 1), 1)
 
 def test_minus_wrong(self):
-        self.assertNotEqual (soustraction(2,1),2)
+        self.assertNotEqual(soustraction(2,1),2)
