b62b8c669324c34f1515eb6ebeb1e2a813db2e6a
unknown <sanaebounajma7@gmail.com>
2023-03-08 14:56:50 +0100

    f

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-def test_minus_true(self):
-        self.assertEqual(soustraction(2, 1), 1)
-
-def test_minus_wrong(self):
-        self.assertNotEqual(soustraction(2,1),2)
+def soustraction(a, b):
+    return a-b
--- tests.py
+++ tests.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 from main import * 
 import pytest
 from unittest import TestCase
+
 def test_minus_true(self):
         self.assertEqual(soustraction(2, 1), 1)
 
 def test_minus_wrong(self):
-=======
-from main import * 
-import pytest
-from unittest import TestCase
-def test_minus_true(self):
-        self.assertEqual(soustraction(2, 1), 1)
-
-def test_minus_wrong(self):
->>>>>>> 84c30a32cfe3246497d5d7b51574a6b5f2953bb9
-        self.assertNotEqual(soustraction(2,1),2)
+        self.assertNotEqual (soustraction(2,1),2)
